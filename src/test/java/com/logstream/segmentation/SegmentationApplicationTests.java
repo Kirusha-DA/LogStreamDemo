95c9f85a4df94839060928945cd5e36b32c4c456
root <root@LAPTOP-HGOPPG3D>
2024-01-06 23:50:49 +0300

    init commit

---
+++ src/test/java/com/logstream/segmentation/SegmentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logstream.segmentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegmentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
